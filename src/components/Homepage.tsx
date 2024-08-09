0f71ea4014b272904b0cc72b39314271addfac9f
POULAMI DAS <poulami.das7604@gmail.com>
2024-08-09 22:23:53 +0530

    fix:homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Countdown from './Countdown';
+
+
 import Vedios from './Vedios';
 import HomeSectionCarousel from './HomeSectionCaraousel';
 

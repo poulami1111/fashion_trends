a6d29f480b30051d4cdd0b924610bd92ab5c11da
POULAMI DAS <poulami.das7604@gmail.com>
2024-08-13 14:18:46 +0530

    fix:pages

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      {/* Your content goes here */}
+    </div>
+  );
+};
+
+export default Page;

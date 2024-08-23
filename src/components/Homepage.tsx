3f59125a0d5e63784d35d9128fa625e037bf0b6c
POULAMI DAS <poulami.das7604@gmail.com>
2024-08-23 18:24:00 +0530

    fix:images

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -14,7 +14,7 @@ const Homepage = () => {
     { name: 'Kids', videoSrc: 'kids-video.mp4' },
     { name: 'Women', videoSrc: 'women-video.mp4' },
     { name: 'Men', videoSrc: 'men-video.mp4' },
-    { name: 'Black', videoSrc: 'black-video.mp4' },
+  
     
    
     { name: 'Home', videoSrc: 'home-video.mp4' },
@@ -32,7 +32,7 @@ const Homepage = () => {
         {sections.map(({ name }) => (
           <div key={name} className="relative my-6 "> {/* Adjusted to include margin for gaps */}
            
-            <HomeSectionCarousel sectionName={name as 'Kids' | 'Women' | 'Black'  |  'Home' | 'Grocery' } />
+            <HomeSectionCarousel sectionName={name as 'Kids' | 'Women' |   'Home' | 'Grocery' } />
           </div>
         ))}
       </div>
